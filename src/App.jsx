86fa7776b875729e06f725ca5dd4ee21ec133dc8
Danea-Gabriel <gabriell.dannea@gmail.com>
2023-07-24 10:24:26 +0300

    Added mock data for json, added main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return <></>;
+  return (
+    <div className="relative h-[750px] w-[380px] rounded-3xl p-5 bg-gray-600">
+      <div></div>
+    </div>
+  );
 };
 
 export default App;
