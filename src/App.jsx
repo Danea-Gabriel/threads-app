64cf451a433badd72171865488fb860ec22c0dce
Danea-Gabriel <gabriell.dannea@gmail.com>
2023-07-26 17:24:33 +0300

    had a clash with tailwindcss , removed it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import PopUp from "./components/PopUp";
 
 const App = () => {
   return (
     <div className="relative h-[750px] w-[380px] rounded-3xl p-5 bg-gray-600">
-      <div></div>
+      <Navbar />
+      <Header />
+      <Feed />
+      <PopUp />
     </div>
   );
 };
